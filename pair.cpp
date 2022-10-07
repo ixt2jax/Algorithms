8454c175b55c1eed5590a37496bdf07245cc2eeb
ixt2jax <115158255+ixt2jax@users.noreply.github.com>
2022-10-07 19:28:23 +0530

    Hacktoberfest 2022

---
+++ pair.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+//{
+    //pair<int ,string > p;
+    //p = make_pair(3,"mani"); //or {3,"mani"}
+    
+    //cout<<p.first<<" "<<p.second<<endl;
+    //return 0;
+
+//}
+// array of pair
+pair<int,int>p_array[3];
+p_array[0]={1,3};
+p_array[1]={3,4};
+p_array[2]={4,4};
+for(int i=0;i<3;i++){
+    cout<<p_array[i].first<<" "<<p_array[i].second<<endl;
+}}
+
+
